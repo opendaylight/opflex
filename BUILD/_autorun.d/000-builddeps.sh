c256b092cedc24746e309b87a94a40b717a68fe0
Alessandro Salvatori <sandr8@gmail.com>
2014-06-11 14:30:34 -0700

    Build dependencies as part of autorun.sh
    Change-Id: Ife82427c0a26921bdf84980e2771fcea909b18ce
    Signed-off-by: Alessandro Salvatori <sandr8@gmail.com>

---
+++ BUILD/_autorun.d/000-builddeps.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#  Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+# 
+#  This program and the accompanying materials are made available under the
+#  terms of the Eclipse Public License v1.0 which accompanies this distribution,
+#  and is available at http://www.eclipse.org/legal/epl-v10.html
+# 
+
+build_dep_libuv() {
+    msgout "INFO" "libuv autogen.sh"
+    sh autogen.sh || die "Can't run libuv's autogen.sh"
+    msgout "INFO" "libuv configure"
+    ./configure || die "Can't run libuv's configure"
+    msgout "INFO" "libuv make"
+    make || die "Can't run libuv's make"
+}
+
+build_dep() {
+
+    dep=${1:-""}
+
+    cd ${dep} || die "Can't enter deps/${dep}"
+
+    msgout "INFO" "${dep}"
+
+    eval build_dep_${dep}
+
+    cd ..
+}
+
+build_deps() {
+
+    cd deps || die "Can't cd into deps/"
+
+    build_dep libuv || die "Can't build libuv"
+
+    cd ..
+}
+
