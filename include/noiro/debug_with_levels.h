d42dbc8f494c9f4dc9f17e2ba9ec75f6c42f0d25
Alessandro Salvatori <sandr8@gmail.com>
2014-06-12 22:27:17 -0700

    give dbug levels strings a centralized storage
    Change-Id: I6968fda41019548f91e52b155c4f8f04640f2383
    Signed-off-by: Alessandro Salvatori <sandr8@gmail.com>

---
+++ include/noiro/debug_with_levels.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+#pragma once
+#ifndef _INCLUDE__NOIRO__POLICY_AGENT__DEBUG_LEVELS_H
+#define _INCLUDE__NOIRO__POLICY_AGENT__DEBUG_LEVELS_H
+
+#include <dbug.h>
+
+extern char const * kDBG_D;
+extern char const * kDBG_I;
+extern char const * kDBG_W;
+extern char const * kDBG_E;
+
+#endif /* _INCLUDE__NOIRO__POLICY_AGENT__DEBUG_LEVELS_H */
