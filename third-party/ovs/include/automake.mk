6bb68866359569c3eb1db505e8009f9f82cc2e29
amittbose <amitbose@gmail.com>
2014-09-03 19:00:42 -0700

    Remove OVS source files
    Removed all OVS related source files except the header files needed.
    Also checking in OVS static libraries used by the agent; these were
    built on 64-bit Fedora 20.
    
    Change-Id: Iba2bca0239dcd6d0d738ac06428317e8dfeba21e
    Signed-off-by: amittbose <amitbose@gmail.com>

--- third-party/ovs/include/automake.mk
+++
@@ -1,5 +0,0 @@
-include include/linux/automake.mk
-include include/openflow/automake.mk
-include include/openvswitch/automake.mk
-include include/sparse/automake.mk
-include include/windows/automake.mk
