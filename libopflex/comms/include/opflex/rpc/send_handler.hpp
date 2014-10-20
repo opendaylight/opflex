9b1a64a584485ca0bc58ef47628c8344334e7bb2
Alessandro Salvatori <sandr8@gmail.com>
2014-10-20 10:54:13 -0700

    send_handler for deque
    Change-Id: Ice6c893df1e224ef474e31f6227093a1d8fbc41b
    Signed-off-by: Alessandro Salvatori <sandr8@gmail.com>

---
+++ libopflex/comms/include/opflex/rpc/send_handler.hpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+#pragma once
+#ifndef _____COMMS__INCLUDE__OPFLEX__RPC__SEND_HANDLER_HPP
+#define _____COMMS__INCLUDE__OPFLEX__RPC__SEND_HANDLER_HPP
+
+#include <rapidjson/rapidjson.h>
+#include <deque>
+
+namespace opflex { namespace rpc { namespace internal {
+
+template <typename Encoding = rapidjson::UTF8<> >
+struct GenericStringQueue {
+
+    typedef typename Encoding::Ch Ch;
+
+    void Put(Ch c) {
+        deque_.push_back(c);
+    }
+
+    void Flush() {}
+
+    void Clear() {
+        deque_.clear();
+    }
+
+    void ShrinkToFit() {
+        deque_.shrink_to_fit();
+    }
+
+    size_t GetSize() const {
+        return deque_.size();
+    }
+
+    std::deque<Ch> deque_;
+};
+
+//! String buffer with UTF8 encoding
+typedef GenericStringQueue<rapidjson::UTF8<> > StringQueue;
+
+}}}
+
+#endif /* _____COMMS__INCLUDE__OPFLEX__RPC__SEND_HANDLER_HPP */
