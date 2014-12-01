opendaylight/opflex

commit 951cf43ce006045abc3a00e0ce489f4f64a74c0b

cleanup includes (local << utilities << dependencies << boost << STL << CRT)
Change-Id: Id4a2edc878c4a021a8c9146c0489c8658fbd9c4d
Signed-off-by: Alessandro Salvatori <sandr8@gmail.com>

libopflex/comms/debug_trace.cpp
@@ -7,9 +7,9 @@
  */
 
 #include <opflex/logging/internal/logging.hpp>
-#include <stdlib.h>
-#include <execinfo.h>
+
 #include <cxxabi.h>
+#include <execinfo.h>
 
 extern "C" void __cyg_profile_func_enter(void *this_fn, void *call_site)
                                         __attribute__((no_instrument_function));

libopflex/comms/active_connection.cpp
@@ -6,8 +6,8 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-#include <yajr/internal/comms.hpp>
 #include <opflex/logging/internal/logging.hpp>
+#include <yajr/internal/comms.hpp>
 
 /*
                              _        _   _

libopflex/comms/common.cpp
@@ -6,14 +6,13 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-#include <uv.h>
-#include <cstdlib>
+#include <yajr/rpc/rpc.hpp>
 #include <yajr/internal/comms.hpp>
+
 #include <opflex/logging/internal/logging.hpp>
 #include <boost/scoped_ptr.hpp>
-#include <yajr/rpc/rpc.hpp>
 
-#include <cstdio>
+#include <uv.h>
 
 int ::yajr::initLoop(uv_loop_t * loop) {
 

libopflex/comms/include/yajr/internal/comms.hpp
@@ -10,15 +10,18 @@
 #ifndef _INCLUDE__OPFLEX__COMMS_INTERNAL_HPP
 #define _INCLUDE__OPFLEX__COMMS_INTERNAL_HPP
 
-#include <cstdlib>  /* for malloc() and free() */
-#include <sstream>  /* for basic_stringstream<> */
-#include <yajr/yajr.hpp>
-#include <rapidjson/document.h>
-#include <yajr/rpc/rpc.hpp>
-#include <yajr/rpc/message_factory.hpp>
 #include <yajr/rpc/send_handler.hpp>
+#include <yajr/rpc/message_factory.hpp>
+#include <yajr/yajr.hpp>
+#include <yajr/rpc/rpc.hpp>
+
+#include <rapidjson/document.h>
 #include <iovec-utils.hh>
+
 #include <boost/intrusive/list.hpp>
+
+#include <sstream>  /* for basic_stringstream<> */
+#include <iostream>
 
 #ifndef NDEBUG
 #  include <boost/version.hpp>
@@ -27,8 +30,6 @@
 #    include <boost/atomic.hpp>
 #  endif
 #endif
-
-#include <iostream>
 
 namespace yajr { namespace comms { namespace internal {
 

libopflex/comms/include/yajr/rpc/internal/json_stream_wrappers.hpp
@@ -10,6 +10,8 @@
 #ifndef _____COMMS__INCLUDE__OPFLEX__COMMS__INTERNAL__JSON_HPP
 #define _____COMMS__INCLUDE__OPFLEX__COMMS__INTERNAL__JSON_HPP
 
+#include <istream>
+
 #include <rapidjson/rapidjson.h>
 
 namespace yajr { namespace comms { namespace internal { namespace wrapper {

libopflex/comms/include/yajr/rpc/message_factory.hpp
@@ -10,12 +10,15 @@
 #ifndef _COMMS__INCLUDE__OPFLEX__RPC__MESSAGE_FACTORY_HPP
 #define _COMMS__INCLUDE__OPFLEX__RPC__MESSAGE_FACTORY_HPP
 
-#include <rapidjson/document.h>
-#include <opflex/logging/internal/logging.hpp>
-#include <yajr/internal/comms.hpp>
-#include <boost/function.hpp>
-#include <rapidjson/writer.h>
 #include <yajr/rpc/send_handler.hpp>
+#include <yajr/internal/comms.hpp>
+
+#include <opflex/logging/internal/logging.hpp>
+
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+
+#include <boost/function.hpp>
 
 namespace yajr {
 

libopflex/comms/include/yajr/rpc/methods.hpp
@@ -10,10 +10,11 @@
 #ifndef _____COMMS__INCLUDE__YAJR__RPC__METHODS_HPP
 #define _____COMMS__INCLUDE__YAJR__RPC__METHODS_HPP
 
-#include <rapidjson/document.h>
-#include <yajr/yajr.hpp>
 #include <yajr/internal/comms.hpp> // for nextId()
 #include <yajr/rpc/rpc.hpp>
+#include <yajr/yajr.hpp>
+
+#include <rapidjson/document.h>
 
 namespace yajr {
 

libopflex/comms/include/yajr/rpc/rpc.hpp
@@ -10,16 +10,21 @@
 #ifndef _____COMMS__INCLUDE__OPFLEX__RPC_HPP
 #define _____COMMS__INCLUDE__OPFLEX__RPC_HPP
 
-#include <rapidjson/document.h>
+#include <yajr/rpc/send_handler.hpp>
+#include <yajr/yajr.hpp>
+
 #include <opflex/logging/internal/logging.hpp>
+
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+
+#include <uv.h>
+
 #include <boost/intrusive/set.hpp>
 #include <boost/functional/hash.hpp>
 #include <boost/scoped_ptr.hpp>
-#include <uv.h>
-#include <rapidjson/writer.h>
 #include <boost/function.hpp>
-#include <yajr/rpc/send_handler.hpp>
-#include <yajr/yajr.hpp>
+
 #include <deque>
 
 namespace yajr {
@@ -651,7 +656,5 @@ std::size_t hash_value(yajr::rpc::LocalIdentifier const & id);
 std::size_t hash_value(yajr::rpc::RemoteIdentifier const & id);
 
 }}
-
-#include <yajr/rpc/message_factory.hpp>
 
 #endif/*_____COMMS__INCLUDE__OPFLEX__RPC_HPP*/

commit 52c47701d9703e65231067d2b02c910a48b4f6ae

Fix build breakage introduced by 951cf43ce
Change-Id: I3c935d3caa7ec768f3a8c27a19a8b144f11ee312
Signed-off-by: Rob Adams <readams@readams.net>

libopflex/comms/debug_trace.cpp
@@ -6,6 +6,8 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
+#include <stdlib.h>
+
 #include <opflex/logging/internal/logging.hpp>
 
 #include <cxxabi.h>