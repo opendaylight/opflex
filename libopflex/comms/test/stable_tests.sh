c5bd371f570f5e2977e8191b1d7ad5b69c7c0d09
Alessandro Salvatori <sandr8@gmail.com>
2014-12-05 09:37:00 -0800

    kill test watcher when tests end by themself
    Change-Id: Icd31bf06dc02d58a53d1813b0f114e78ccfd5c87
    Signed-off-by: Alessandro Salvatori <sandr8@gmail.com>

--- libopflex/comms/test/stable_tests.sh
+++ libopflex/comms/test/stable_tests.sh
@@ -10,7 +10,7 @@ declare -i interval=5
 # Delay between posting the SIGTERM signal and destroying the process by SIGKILL.
 declare -i delay=2
 
-(
+function watcher() {
     ((t = timeout))
 
     while ((t > 0)); do
@@ -24,9 +24,16 @@ declare -i delay=2
     kill -s SIGTERM $$ && kill -0 $$ || exit 0
     sleep $delay
     kill -s SIGKILL $$
-) 2> /dev/null &
+}
+
+(watcher) &> /dev/null &
+BG_WATCHER=$!
 
 ./comms_test --catch_system_errors=no --run_test=asynchronous_sockets/${CATEGORY}_test_'*'
+RET_VAL=$?
+kill -9 ${BG_WATCHER} &> /dev/null
 
 # FIXME: try to parse core pattern
 find . -name '*core*' -type f -exec echo Found possible core file: '{}' \;
+
+exit ${RET_VAL}
