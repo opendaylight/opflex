15fbd85098136b2f93f94742f74345b5c965f3d8
Deepjyoti Kakati <dkakati73@yahoo.com>
2015-02-16 16:24:16 +0530

    identify and peer listener functions
    - set the identify of the framework
    - create a peer status listener object and save two C function
      callbacks therein
    - a peerstatus register function which takes framework and the newly
      create peer status listener obj and registers that for the framework
    - a destroy function for the peer status listener object, this is to be
      called just before the framework is destroyed
    - implementation of the member functions of peer status listener object
      to take the peer connection status and connection pool health callabcks
      by the MODB, and call the two provided C callback functions as appropriate.
      the two C callback functions should implement the user activity of the
      listener side.
    
    Change-Id: I0ad42d2c150e8d20d7a58ce5364a15bcd0a9d1d3
    Signed-off-by: Deepjyoti Kakati <dkakati73@yahoo.com>

--- libopflex/include/opflex/c/offramework_c.h
+++ libopflex/include/opflex/c/offramework_c.h
@@ -32,6 +32,47 @@
  */
 
 /**
+ * The peer is disconnected and not trying to connect
+ */
+#define OF_PEERSTATUS_DISCONNECTED 0
+/**
+ * The peer is connecting
+ */
+#define OF_PEERSTATUS_CONNECTING   1
+/**
+ * The peer is connected but not yet ready
+ */
+#define OF_PEERSTATUS_CONNECTED    2
+/**
+ * The peer is connected and ready
+ */
+#define OF_PEERSTATUS_READY        3
+/**
+ * The peer connection is closing 
+ */
+#define OF_PEERSTATUS_CLOSING      4
+/**
+ * error condition, invalid state
+ */
+#define OF_PEERSTATUS_ERROR        -1
+
+/**
+ * There is no ready Opflex peer connection
+ */
+#define OF_CONNHEALTH_DOWN         0
+/**
+ * At least one opflex peer is in a state other than READY
+ */
+#define OF_CONNHEALTH_DEGRADED     1
+/**
+ * All opflex peers are connected and ready 
+ */
+#define OF_CONNHEALTH_HEALTHY      2
+/**
+ * error condition, invalid state
+ */
+#define OF_CONNHEALTH_ERROR        -1
+/**
  * A pointer to a generated model metadata object.  This object is
  * entirely opaque and appears in the wrapper interface as just a
  * pointer.
@@ -52,8 +93,14 @@ typedef ofobj_p ofmetadata_p;
  * A pointer to an OF framework object
  */
 typedef ofobj_p offramework_p;
+/**
+ * A pointer to an CPeerStatusListener object
+ */
+typedef ofobj_p ofpeerstatuslistener_p;
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 
     /**
      * Create a new OF framework instance.  You must eventually call
@@ -86,6 +133,20 @@ extern "C" {
                                    ofmetadata_p metadata);
 
     /**
+     * Set the opflex identity information for this framework
+     * instance.
+     *
+     * @param framework the framework
+     * @param name the unique name for this opflex component within
+     * the policy domain
+     * @param domain the globally unique name for this policy domain
+     * @return a status code
+     */
+    ofstatus offramework_set_opflex_identity(offramework_p framework,
+                                             const char* name,
+                                             const char* domain);
+
+    /**
      * Start the framework.  This will start all the framework threads
      * and attempt to connect to configured OpFlex peers.
      *
@@ -102,7 +163,78 @@ extern "C" {
      */
     ofstatus offramework_stop(offramework_p framework);
 
+    /**
+     * Add an OpFlex peer. If the framework is started, this will
+     * immediately initiate a new connection asynchronously.
+     *
+     * When connecting to the peer, that peer may provide an
+     * additional list of peers to connect to, which will be
+     * automatically added as peers. If the peer does not include
+     * itself in the list, then the framework will disconnect from
+     * that peer and add the peers in the list. In this way, it is
+     * possible to automatically bootstrap the correct set of peers
+     * using a known hostname or IP address or a known, fixed anycast
+     * IP address.
+     *
+     * @param framework the framework
+     * @param hostname the hostname or IP address to connect to
+     * @param port the TCP port to connect on
+     * @return a status code
+     */
+    ofstatus offramework_add_peer(offramework_p framework,
+                                  const char* hostname, int port);
+
+    /** 
+     * callback function in C caller code for handling the peer connection
+     * status health callback from modb
+     */
+    typedef void (*ofpeerconnstatus_callback_p)(const char *peerhostname, 
+                                                int port, 
+                                                int peerconn_status_code);
+    /** 
+     * callback function in C caller code for handling overall connection 
+     * pool health callback from modb
+     */
+    typedef void (*ofconnpoolhealth_callback_p)(
+                                              int connpoolhealth_status_code);
+
+    /**
+     * create a object of type CPeerStatusListener (see offramework.cpp) 
+     * to be used for registering with the framework later . Store the 
+     * provided callbacks as class variable to be used in the member functions
+     * later
+     * @param obj return the pointer to created object
+     * @param cpeerconnstatus_callback caller provided function pointer for 
+     *        handling status callback of any peer
+     * @param cconnpoolhealth_callback caller provided functional pointer for
+     *        handling peer connection pool health callback
+     * @return a status code
+     */
+    ofstatus offramework_create_peerstatus_listener(
+                         ofpeerstatuslistener_p *obj,
+                         ofpeerconnstatus_callback_p cpeerconnstatus_callback,
+                         ofconnpoolhealth_callback_p cconnpoolhealth_callback);
+    /**
+     * register to listen to opflex peer connection status and health
+     * @param framework the framework
+     * @param obj this is a dynamically created object of class
+     *        CPeerStatusListener passed in by caller.eee offramework.cpp
+     * @return a status code 
+     */
+    ofstatus offramework_register_peerstatus_listener(offramework_p framework,
+                                                   ofpeerstatuslistener_p obj);
+    /**
+     * destroy the PeerStatusListener object used in register function earlier
+     *
+     * @param obj Object of type CPeerStatusListener
+     * @return a status code
+     */
+    ofstatus offramework_destroy_peerstatus_listener(
+                                              ofpeerstatuslistener_p *obj);
+
+#ifdef __cplusplus
 } /* extern "C" */
+#endif
 
 /** @} offramework */
 /** @} ccore */
