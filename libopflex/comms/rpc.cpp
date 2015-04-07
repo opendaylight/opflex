5c277f2ad31d64faa49ccf1b089bd0b840c8fc81
Alessandro Salvatori <sandr8@gmail.com>
2015-04-07 15:56:41 +0200

    add support for custom method and reorganize handlers
    Change-Id: I3c9f56a1c84ad72a707ea0973bcc3497d1ae467c
    Signed-off-by: Alessandro Salvatori <sandr8@gmail.com>

--- libopflex/comms/rpc.cpp
+++ libopflex/comms/rpc.cpp
@@ -352,6 +352,7 @@ MethodName method::endpoint_resolve("endpoint_resolve");
 MethodName method::endpoint_unresolve("endpoint_unresolve");
 MethodName method::endpoint_update("endpoint_update");
 MethodName method::state_report("state_report");
+MethodName method::custom("custom");
 
 } /* yajr::rpc namespace */
 } /* yajr namespace */
