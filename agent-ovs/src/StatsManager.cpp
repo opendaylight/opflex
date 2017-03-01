10d5651e088b95cb46de7c6ab32dc3eaaf14e8d1
Vinod Chamunny <vinodcy@gmail.com>
2017-03-01 14:45:40 -0800

    Stats collection for interface
    Change-Id: Ia357488f66147a0a91164cf39169a4085ed23ae6
    Signed-off-by: Vinod Chamunny <vinodcy@gmail.com>

--- agent-ovs/src/StatsManager.cpp
+++ agent-ovs/src/StatsManager.cpp
@@ -30,7 +30,8 @@ using boost::system::error_code;
 
 StatsManager::StatsManager(Agent* agent_, PortMapper& portMapper_,
                            long timer_interval_)
-    : agent(agent_), portMapper(portMapper_), connection(NULL),
+    : agent(agent_), portMapper(portMapper_),
+      intConnection(NULL), accessConnection(NULL),
       agent_io(agent_->getAgentIOService()),
       timer_interval(timer_interval_), stopping(false) {
 
@@ -40,15 +41,20 @@ StatsManager::~StatsManager() {
 
 }
 
-void StatsManager::registerConnection(SwitchConnection* connection) {
-    this->connection = connection;
+void StatsManager::registerConnection(SwitchConnection* intConnection, SwitchConnection *accessConnection ) {
+    this->intConnection = intConnection;
+    this->accessConnection = accessConnection;
+    if (accessConnection != NULL) {
+        this->numConnections = 2;
+    }
 }
 
 void StatsManager::start() {
     LOG(DEBUG) << "Starting stats manager";
     stopping = false;
 
-    connection->RegisterMessageHandler(OFPTYPE_PORT_STATS_REPLY, this);
+    intConnection->RegisterMessageHandler(OFPTYPE_PORT_STATS_REPLY, this);
+    accessConnection->RegisterMessageHandler(OFPTYPE_PORT_STATS_REPLY, this);
 
     timer.reset(new deadline_timer(agent_io, milliseconds(timer_interval)));
     timer->async_wait(bind(&StatsManager::on_timer, this, error));
@@ -58,8 +64,11 @@ void StatsManager::stop() {
     LOG(DEBUG) << "Stopping stats manager";
     stopping = true;
 
-    if (connection) {
-        connection->UnregisterMessageHandler(OFPTYPE_PORT_STATS_REPLY, this);
+    if (intConnection) {
+        intConnection->UnregisterMessageHandler(OFPTYPE_PORT_STATS_REPLY, this);
+    }
+    if (accessConnection) {
+        accessConnection->UnregisterMessageHandler(OFPTYPE_PORT_STATS_REPLY, this);
     }
 
     if (timer) {
@@ -75,9 +84,18 @@ void StatsManager::on_timer(const error_code& ec) {
     }
 
     // send port stats request
-    struct ofpbuf *portStatsReq = ofputil_encode_dump_ports_request(
-        (ofp_version)connection->GetProtocolVersion(), OFPP_ANY);
-    int err = connection->SendMessage(portStatsReq);
+    struct ofpbuf *intPortStatsReq = ofputil_encode_dump_ports_request(
+        (ofp_version)intConnection->GetProtocolVersion(), OFPP_ANY);
+    int err = intConnection->SendMessage(intPortStatsReq);
+    if (err != 0) {
+        LOG(ERROR) << "Failed to send port statistics request: "
+                   << ovs_strerror(err);
+    }
+
+    // send port stats request
+    struct ofpbuf *accessPortStatsReq = ofputil_encode_dump_ports_request(
+        (ofp_version)accessConnection->GetProtocolVersion(), OFPP_ANY);
+    err = accessConnection->SendMessage(accessPortStatsReq);
     if (err != 0) {
         LOG(ERROR) << "Failed to send port statistics request: "
                    << ovs_strerror(err);
@@ -90,38 +108,65 @@ void StatsManager::on_timer(const error_code& ec) {
 }
 
 
-void StatsManager::Handle(SwitchConnection*,
+void StatsManager::Handle(SwitchConnection* connection,
                           int msgType, ofpbuf *msg) {
     assert(msgType == OFPTYPE_PORT_STATS_REPLY);
 
     const struct ofp_header *oh = (ofp_header *)msg->data;
     struct ofputil_port_stats ps;
     struct ofpbuf b;
+    static EndpointManager::EpCounters counters;
+    static bool intBridgeReply = false;
+    static bool accessBridgeReply = false;
+    if (connection->getSwitchName().compare("br-int") == 0) {
+        intBridgeReply = true;
+        if (numConnections > 1 && accessBridgeReply == false) {
+            memset(&counters, 0, sizeof(counters));
+        }
+
+    } else {
+        accessBridgeReply = true;
+        if (numConnections > 1 && intBridgeReply == false) {
+            memset(&counters, 0, sizeof(counters));
+        }
+    }
 
     ofpbuf_use_const(&b, oh, ntohs(oh->length));
     ofpraw_pull_assert(&b);
     while (!ofputil_decode_port_stats(&ps, &b)) {
-        EndpointManager::EpCounters counters;
-        memset(&counters, 0, sizeof(counters));
-
-        counters.txPackets = ps.stats.tx_packets;
-        counters.rxPackets = ps.stats.rx_packets;
-        counters.txBytes = ps.stats.tx_bytes;
-        counters.rxBytes = ps.stats.rx_bytes;
-        counters.txDrop = ps.stats.tx_dropped;
-        counters.rxDrop = ps.stats.rx_dropped;
-
-        EndpointManager& epMgr = agent->getEndpointManager();
-        std::unordered_set<std::string> endpoints;
-        try {
-            const std::string& portName = portMapper.FindPort(ps.port_no);
-            epMgr.getEndpointsByIface(portName, endpoints);
-        } catch (std::out_of_range e) {
-            // port not known yet
+       if (intBridgeReply) {
+           counters.txPackets = ps.stats.tx_packets;
+           counters.txBytes = ps.stats.tx_bytes;
+           if (numConnections == 1) {
+               counters.rxPackets = ps.stats.rx_packets;
+               counters.rxBytes = ps.stats.rx_bytes;
+           }
+           counters.txDrop = counters.txDrop + ps.stats.tx_dropped;
+           counters.rxDrop = counters.rxDrop + ps.stats.rx_dropped;
+        } else {
+           counters.rxPackets = ps.stats.rx_packets;
+           counters.rxBytes = ps.stats.rx_bytes;
+           counters.txDrop = counters.txDrop + ps.stats.tx_dropped;
+           counters.rxDrop = counters.rxDrop + ps.stats.rx_dropped;
         }
 
-        for (const std::string& uuid : endpoints) {
-            epMgr.updateEndpointCounters(uuid, counters);
+       if ((numConnections  == 1 && intBridgeReply) ||
+         (numConnections > 1 && accessBridgeReply && intBridgeReply)) {
+          EndpointManager& epMgr = agent->getEndpointManager();
+          std::unordered_set<std::string> endpoints;
+          try {
+              const std::string& portName = portMapper.FindPort(ps.port_no);
+              epMgr.getEndpointsByIface(portName, endpoints);
+          } catch (std::out_of_range e) {
+           // port not known yet
+          }
+
+          for (const std::string& uuid : endpoints) {
+               epMgr.updateEndpointCounters(uuid, counters);
+          }
+          memset(&counters, 0, sizeof(counters));
+          intBridgeReply  = false;
+          accessBridgeReply  = false;
         }
     }
 }
