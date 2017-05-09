ccb22a5d1e8994e3c31ea870babcf2ed0b9d414f
Keith Burns (alagalah) <alagalah@gmail.com>
2017-05-09 16:05:40 -0700

    Initial commit for VPP renderer
    ./configure --enable-renderer-vpp=yes --enable-renderer-ovs=no
    
    Change-Id: I1499e84177e211873c519571b7d861f0d4fa4969
    Signed-off-by: Keith Burns (alagalah) <alagalah@gmail.com>
    Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

--- agent-ovs/src/Agent.cpp
+++ agent-ovs/src/Agent.cpp
@@ -28,6 +28,10 @@
 #include "StitchedModeRenderer.h"
 #endif
 
+#ifdef RENDERER_VPP
+#include "VppRenderer.h"
+#endif
+
 #include <unordered_map>
 
 namespace ovsagent {
@@ -84,6 +88,8 @@ void Agent::setProperties(const boost::property_tree::ptree& properties) {
 
     static const std::string RENDERERS_STITCHED_MODE("renderers.stitched-mode");
     static const std::string RENDERERS_OPENVSWITCH("renderers.openvswitch");
+
+    static const std::string RENDERERS_VPP("renderers.vpp");
 
     optional<std::string> logLvl =
         properties.get_optional<std::string>(LOG_LEVEL);
@@ -178,6 +184,12 @@ void Agent::setProperties(const boost::property_tree::ptree& properties) {
 #else
         (RENDERERS_STITCHED_MODE, disabled_create)
         (RENDERERS_OPENVSWITCH, disabled_create)
+#endif
+
+#ifdef RENDERER_VPP
+        (RENDERERS_VPP, VppRenderer::create)
+#else
+        (RENDERERS_VPP, disabled_create)
 #endif
         ;
 
---
+++ agent-ovs/src/include/VppRenderer.h
@@ -0,0 +1,79 @@
+/* -*- C++ -*-; c-basic-offset: 4; indent-tabs-mode: nil */
+/*
+ * Include file for Renderer
+ *
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+#include <boost/property_tree/ptree.hpp>
+#include <opflex/ofcore/OFFramework.h>
+#include "Renderer.h"
+#include "VppManager.h"
+#include "IdGenerator.h"
+
+#pragma once
+#ifndef OVSAGENT_VPP_RENDERER_H
+#define OVSAGENT_VPP_RENDERER_H
+
+namespace ovsagent {
+
+/**
+ * Vpp renderer is the component that actually enforces the policy.
+ * For example, a renderer might enforce the policy to a local bridge, by
+ * creating ACLs and route entries in Vpp.
+ */
+class VppRenderer : public Renderer {
+public:
+    /**
+     * Instantiate a vpp renderer
+     *
+     * @param agent the agent object
+     */
+    VppRenderer(Agent& agent);
+
+    /**
+     * Destroy the renderer and clean up all state
+     */
+    virtual ~VppRenderer();
+
+    /**
+     * Allocate a vpp renderer on the heap
+     *
+     * @return a pointer to the renderer.  The memory is owned by the
+     * caller
+     */
+    static Renderer* create(Agent& agent);
+
+    /**
+     * Configure the renderer with the property tree specified, which
+     * will be a subtree in the base agent configuration
+     *
+     * @param properties the configuration properties to set for the
+     * agent
+     */
+    virtual void setProperties(const boost::property_tree::ptree& properties);
+
+    /**
+     * Start the renderer
+     */
+    virtual void start();
+
+    /**
+     * Stop the renderer
+     */
+    virtual void stop();
+
+private:
+    IdGenerator idGen;
+    VppManager vppManager;
+    std::string bridgeName;
+    bool started;
+};
+
+} /* namespace ovsagent */
+
+#endif /* OVSAGENT_VPP_RENDERER_H */
