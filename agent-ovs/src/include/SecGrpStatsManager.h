555736dd00d857f98998d65aea37be0ed63877f6
maragupa <mailmanoj2005@gmail.com>
2017-06-16 14:56:39 -0700

    Use unique_ptr for CircularBuffer Removed deriving from Policy Listener Add units test for circular buffer handling and counter object deletion after sec grp and contract delete Add L24Classifier Listener to Contract and SecGrp stats manager Use vectors and structs to track counter objects Added listeners to detect contract and sec grp deletions and clear the gbpe objects Added a circular buffer to store gbpe objects and clear them when full Moved counter flow aps to derived class and use methods from base class to access them Further filtered out common code from AccStatsManager and PolicyStatsManager Refactored unit test codes and reuse most of it for PolicyStatsManager and AccStatsManager Added AccStatsManager to collect stats from accesbridge Created a base class for AccStatsManager and PolicyStatsManager to keep common code Added 4 unit tests for AccStatsManager Added a new genie model HPPClassifierCounter Fixed AccessFlowManager_test with new change - Rule as cookie instead of secGrpId Change-Id: I9e209e57eec1bb31a5831283be5d947bb2d0d298 Signed-off-by: maragupa <mailmanoj2005@gmail.com>

---
+++ agent-ovs/src/include/SecGrpStatsManager.h
@@ -0,0 +1,110 @@
+/* -*- C++ -*-; c-basic-offset: 4; indent-tabs-mode: nil */
+/*
+ * Include file for policy stats manager
+ *
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+
+#include "BaseStatsManager.h"
+
+#pragma once
+#ifndef OVSAGENT_SecGrpStatsManager_H
+#define OVSAGENT_SecGrpStatsManager_H
+
+namespace ovsagent {
+
+class Agent;
+
+/**
+ * Periodically query an OpenFlow switch for policy counters and stats
+ * and distribute them as needed to other components for reporting.
+ */
+class SecGrpStatsManager : public BaseStatsManager {
+public:
+    /**
+     * Instantiate a new policy stats manager that will use the
+     * provided io service for scheduling asynchronous tasks
+     * @param agent the agent associated with the Policy Stats Manager
+     * @param idGen the ID generator
+     * @param switchManager the switchManager associated with the
+     * Policy Stats Manager
+     * @param idGen the flow ID generator
+     * @param timer_interval the interval for the stats timer in
+     * milliseconds
+     */
+    SecGrpStatsManager(Agent* agent, IdGenerator& idGen,
+                       SwitchManager& switchManager,
+                       long timer_interval = 30000);
+
+    /**
+     * Destroy the policy stats manager and clean up all state
+     */
+    ~SecGrpStatsManager();
+
+    /**
+     * Start the policy stats manager
+     */
+    void start();
+
+    /**
+     * Stop the policy stats manager
+     */
+    void stop();
+
+    /**
+     * Timer interval handler.  For unit tests only.
+     */
+    void on_timer(const boost::system::error_code& ec);
+    /** Check to see if the Stats Manager Object can handle the table_id
+     * @return true or false
+     */
+    bool isTableIdFound(uint8_t table_id);
+
+    /* Policy Listener call back to capture when 
+     * contract is created or deleted
+     * */
+    void resolveCounterMaps(uint8_t table_id);
+
+    /* modb object listener for L24 classifier object
+     * If the object is deleted, delete all 
+     * the associated counter objects to prevent memory leak
+     * */
+    void objectUpdated(opflex::modb::class_id_t class_id,
+        const opflex::modb::URI& uri);
+
+    /* Derived class API to delete indexth counter object
+     * */
+    void clearCounterObject(const std::string& key,uint8_t index);
+    
+
+private:
+    flowCounterState_t secGrpInState;
+    flowCounterState_t secGrpOutState;
+    
+    void updateAccInFlowEntryMap(uint64_t cookie, uint16_t priority,
+                            const struct match& match);
+    void updateAccOutFlowEntryMap(uint64_t cookie, uint16_t priority,
+                            const struct match& match);
+    void updatePolicyStatsCounters(const std::string& l24Classifier,
+                          PolicyCounters_t& newVals1,
+                          PolicyCounters_t& newVals2);
+
+    void handleDropStats(uint32_t rdId,
+                         boost::optional<std::string> idRdStr,
+                         struct ofputil_flow_stats* fentry) {
+    }
+    void updatePolicyStatsCounters(const std::string& srcEpg,
+                                   const std::string& dstEpg,
+                                   const std::string& ruleURI,
+                                   PolicyCounters_t& counters) {
+    }
+};
+
+} /* namespace ovsagent */
+
+#endif /* OVSAGENT_SecGrpStatsManager_H */
