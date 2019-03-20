044d99581b6756afc6d48ecc84a5965e91ae76a0
tarbash <waris12@yahoo.com>
2019-03-20 10:23:02 -0700

    Enhanced genie model to 1. add a session container for all span artifacts 2. add a reference to span/session instances from EPG
    Assumption: opflex proxy will add a reference to EPG pointing to
    the relevant span/sessions.
    When End points are declared, any references to EPGs will be resolved
    and any references from the EPG to other artifacts will be resolved.
    On removal of an end point, the EPG is updated by opflex proxy to add/remove
    span/session references.
    Added span manager to manage span artifacts and logic.
    Updated Policies.cpp to generate span artifacts for mock server
    and verified that span artifacts are indeed created and resolved by the agent.
    
    Change-Id: I42d18fb49ee3e18c6b19f123ef79ce3a5c11c474
    Signed-off-by: tarbash <waris12@yahoo.com>

---
+++ agent-ovs/lib/include/opflexagent/SpanManager.h
@@ -0,0 +1,92 @@
+/* -*- C++ -*-; c-basic-offset: 4; indent-tabs-mode: nil */
+/*
+ * Include file for SpanManager listener
+ *
+ * Copyright (c) 2019 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+#pragma once
+#ifndef OPFLEXAGENT_SPANMANAGER_H
+#define OPFLEXAGENT_SPANMANAGER_H
+
+#include <opflex/ofcore/OFFramework.h>
+#include <opflex/modb/ObjectListener.h>
+#include <modelgbp/span/Universe.hpp>
+#include <boost/asio.hpp>
+#include <thread>
+
+using boost::asio::deadline_timer;
+
+namespace opflexagent {
+
+/**
+ * An abstract interface for classes interested in updates related to
+ * the span artifacts
+ */
+class SpanManager {
+public:
+    /**
+     * Instantiate a new span manager
+     */
+    SpanManager(opflex::ofcore::OFFramework& framework_);
+
+    /**
+     * Destroy the span manager and clean up all state
+     */
+    ~SpanManager() {};
+
+    /**
+     * Start the span manager
+     */
+    void start();
+
+    /**
+     * Stop the span manager
+     */
+    void stop();
+    /**
+     * called when span timer expires. It invokes the update method for span artifacts.
+     * @param[in] ec error code.
+     */
+    void on_timer_span(const boost::system::error_code& ec);
+
+    /**
+     * Listener for changes related to span
+     */
+    class SpanUniverseListener : public opflex::modb::ObjectListener {
+    public:
+        /**
+         * constructor for SpanUniverseListener
+         * @param[in] spanmanager reference to span manager
+         */
+        SpanUniverseListener(SpanManager& spanmanager);
+        virtual ~SpanUniverseListener();
+
+        /**
+         * callback for handling updates to Span universe
+         * @param[in] class_id class id of updated object
+         * @param[in] uri of updated object
+         */
+        virtual void objectUpdated(opflex::modb::class_id_t class_id,
+                                   const opflex::modb::URI& uri);
+    private:
+        SpanManager& spanmanager;
+    };
+
+    /**
+     * instance of span universe listener class.
+     */
+    SpanUniverseListener spanUniverseListener;
+    friend class SpanUniverseListener;
+
+private:
+    opflex::ofcore::OFFramework& framework;
+};
+}
+
+
+#endif /* OPFLEXAGENT_SPANMANAGER_H */
