020f2c2d0a1f23290f837194f7b954a969be0f26
deepmanit <deep.manit@gmail.com>
2019-10-30 15:38:36 +0530

    code addeded for ovs netflow
    Signed-off-by: Deepesh Deshmukh <deep.manit@gmail.com>
    Change-Id: I4422ef37f1bfc054c84d2943b64d012e6a191175

---
+++ agent-ovs/lib/include/opflexagent/NetFlowListener.h
@@ -0,0 +1,50 @@
+/* -*- C++ -*-; c-basic-offset: 4; indent-tabs-mode: nil */
+/*
+ * Include file for netflow Listener
+ *
+ * Copyright (c) 2019 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+#pragma once
+#ifndef OPFLEX_NETFOWLISTENER_H
+#define OPFLEX_NETFOWLISTENER_H
+
+#include <opflexagent/ExporterConfigState.h>
+
+namespace opflexagent {
+
+using namespace std;
+
+/**
+ * class defining api for listening to net flow updates
+ */
+class NetFlowListener  {
+public:
+    /**
+     * destroy netflow listener and clean up all state.
+     */
+    virtual ~NetFlowListener() {};
+
+    /**
+     * called when netflow exporterconfig has been deleted
+     * @param[in] exporterconfigSt shared pointer to exporterconfig object
+     */
+    virtual void netflowDeleted(shared_ptr<ExporterConfigState> exporterconfigSt) {};
+
+    /**
+     * Called when netflow objects are updated.
+     */
+     virtual void netflowUpdated(const opflex::modb::URI&) {}
+
+     /**
+      * delete all netflow config
+      */
+     virtual void netflowDeleted() {};
+
+};
+}
+#endif // OPFLEX_NETFOWLISTENER_H
