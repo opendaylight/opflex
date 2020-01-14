8c7980f0d76d1f814c9487b8104c98b100163c97
gautvenk <gautam.chennai@gmail.com>
2020-01-13 20:15:13 -0800

    Prometheus: Making port #9612 bind across any available IPv4 address
    Signed-off-by: Gautam Venkataramanan <gautam.chennai@gmail.com>
    Change-Id: I3eb65d7ff095a6b9406a8f0525d3372ff67cb569

--- agent-ovs/lib/PrometheusManager.cpp
+++ agent-ovs/lib/PrometheusManager.cpp
@@ -235,7 +235,7 @@ void PrometheusManager::start()
      * Note: Port #9612 has been reserved for opflex here:
      * https://github.com/prometheus/prometheus/wiki/Default-port-allocations
      */
-    exposer_ptr = unique_ptr<Exposer>(new Exposer{"127.0.0.1:9612", "/metrics", 1});
+    exposer_ptr = unique_ptr<Exposer>(new Exposer{"9612", "/metrics", 1});
     registry_ptr = make_shared<Registry>();
 
     /* Initialize Metric families which can be created during
