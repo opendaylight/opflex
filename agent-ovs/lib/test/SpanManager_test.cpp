8bf5a6175e4d7b08ec7f5fa69809a0120e66b718
Mandeep S Sandhu <waris12@yahoo.com>
2020-01-15 20:51:22 -0500

    Added mutex guards for access to Span data structures.
    This fixes issues with tests when the same data is being changed while being read.
    
    Signed-off-by: Mandeep S Sandhu <waris12@yahoo.com>
    Change-Id: I0e47ea93560d122ab1f4797f4a797a34793a0040

--- agent-ovs/lib/test/SpanManager_test.cpp
+++ agent-ovs/lib/test/SpanManager_test.cpp
@@ -10,6 +10,8 @@
  */
 
 #include <list>
+#include <thread>
+#include <chrono>
 #include <boost/test/unit_test.hpp>
 #include <boost/assign/list_of.hpp>
 #include <modelgbp/dmtree/Root.hpp>
@@ -125,10 +127,13 @@ public:
 
         mutatorElem.commit();
 
-        Mutator mutator2(framework, "policyreg");
         lEp1->addSpanLocalEpToEpRSrc()->setTargetL2Ep(l2E1->getURI());
 
-        mutator2.commit();
+        mutator.commit();
+
+        unsigned int delay = 100;
+        LOG(DEBUG) << "sleeping for " << delay << " secs";
+        this_thread::sleep_for(chrono::milliseconds(delay));
 
     }
 
@@ -154,8 +159,10 @@ public:
 BOOST_AUTO_TEST_SUITE(SpanManager_test)
 
 
-static bool checkSpan(boost::optional<shared_ptr<SessionState>> pSess,
+static bool checkSpan(Agent& agent,
                       const URI& spanUri) {
+    boost::optional<shared_ptr<SessionState>> pSess =
+            agent.getSpanManager().getSessionState(spanUri);
     if (!pSess)
         return false;
     if (spanUri == pSess.get()->getUri())
@@ -164,8 +171,10 @@ static bool checkSpan(boost::optional<shared_ptr<SessionState>> pSess,
         return false;
 }
 
-static bool checkSrcEps(boost::optional<shared_ptr<SessionState>> pSess,
+static bool checkSrcEps(Agent& agent, const URI& spanUri,
     shared_ptr<span::SrcMember> srcMem, shared_ptr<L2Ep> l2e) {
+    boost::optional<shared_ptr<SessionState>> pSess =
+            agent.getSpanManager().getSessionState(spanUri);
     if (!pSess) {
         return false;
     }
@@ -188,8 +197,10 @@ static bool checkSrcEps(boost::optional<shared_ptr<SessionState>> pSess,
     return false;
 }
 
-static bool checkDst(boost::optional<shared_ptr<SessionState>> pSess,
+static bool checkDst(Agent& agent, const URI& spanUri,
     shared_ptr<span::DstSummary> dstSumm1) {
+    boost::optional<shared_ptr<SessionState>> pSess =
+            agent.getSpanManager().getSessionState(spanUri);
     if (!pSess) {
         return false;
     }
@@ -219,17 +230,19 @@ static bool testGetSession(shared_ptr<LocalEp> le, optional<URI> uri) {
         return true;
 }
 
+static bool checkEndPoints(Agent& agent, const URI& spanUri, long unsigned int num) {
+    if (agent.getSpanManager().getSessionState(spanUri)
+                .get()->getSrcEndPointSet().size() == num)
+        return true;
+    return false;
+}
+
 BOOST_FIXTURE_TEST_CASE( verify_artifacts, SpanFixture ) {
-    WAIT_FOR(checkSpan(agent.getSpanManager().getSessionState(sess->getURI()),
-            sess->getURI()), 500);
-    WAIT_FOR(agent.getSpanManager().getSessionState(sess->getURI())
-            .get()->getSrcEndPointSet().size() == 2, 500);
-    WAIT_FOR(checkSrcEps(agent.getSpanManager().getSessionState(sess->getURI()),
-            srcMem1, l2E1), 500);
-    WAIT_FOR(checkSrcEps(agent.getSpanManager().getSessionState(sess->getURI()),
-            srcMem2, l2e2), 500);
-    WAIT_FOR(checkDst(agent.getSpanManager().getSessionState(sess->getURI()),
-            dstSumm1), 500);
+    WAIT_FOR(checkSpan(agent, sess->getURI()), 500);
+    WAIT_FOR(checkEndPoints(agent, sess->getURI(), 2), 500);
+    WAIT_FOR(checkSrcEps(agent, sess->getURI(), srcMem1, l2E1), 500);
+    WAIT_FOR(checkSrcEps(agent, sess->getURI(), srcMem2, l2e2), 500);
+    WAIT_FOR(checkDst(agent, sess->getURI(), dstSumm1), 500);
     boost::optional<URI> uri("/SpanUniverse/SpanSession/sess1/");
     BOOST_CHECK(testGetSession(lEp1, uri));
 }
