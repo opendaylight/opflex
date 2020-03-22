3b57229c5a1abb6b07c950aa7a5ca1c466665629
Tom Flynn <tom.flynn@gmail.com>
2020-03-22 12:54:56 -0700

    Minor cleanups in span code
    Signed-off-by: Tom Flynn <tom.flynn@gmail.com>
    Change-Id: I658497790d4ffaf5898c93fee864f0635f1eac42

--- agent-ovs/lib/include/opflexagent/SpanSessionState.h
+++ agent-ovs/lib/include/opflexagent/SpanSessionState.h
@@ -15,6 +15,8 @@
 
 #include <opflex/modb/URI.h>
 #include <modelgbp/gbp/DirectionEnumT.hpp>
+#include <modelgbp/platform/AdminStateEnumT.hpp>
+#include <modelgbp/span/ErspanVersionEnumT.hpp>
 #include <boost/asio.hpp>
 
 namespace  std {
@@ -43,51 +45,10 @@ using namespace std;
 using namespace boost::asio::ip;
 using namespace opflex::modb;
 
-    /**
-     * filter criteria for sourcing traffic
-     */
-    typedef struct {
-        /**
-         * name of filter
-         */
-        string name;
-        /**
-         * ip address to be filtered
-         */
-        address ip;
-        /**
-         * start of port range to be filtered
-         */
-        uint16_t portStart;
-        /**
-         * end pf port range to be filtered
-         */
-        uint16_t portEnd;
-    } filter_t;
-
-    /**
-     * hash function for filter struct to use in sets and maps
-     */
-    struct filter_t_hash {
-        /**
-         * calculate hash of filter
-         * @param k a filter_t struct
-         * @return hash
-         */
-        size_t operator()(const filter_t& k) const {
-            size_t v = 0;
-            boost::hash_combine(v, k.portEnd);
-            boost::hash_combine(v, k.portStart);
-            boost::hash_combine(v, k.name);
-            boost::hash_combine(v, hash<address>()(k.ip));
-            return v;
-        }
-    };
-
-    /**
-     * class to represent a destination end point
-     */
-    class DstEndPoint {
+/**
+ * class to represent a destination end point
+ */
+class DstEndPoint {
     public:
         /**
          * constructor that accepts destination IP address.
@@ -98,17 +59,7 @@ using namespace opflex::modb;
          * copy constructor
          * @param dp reference to object being copied from
          */
-        DstEndPoint(const DstEndPoint& dp) : name(dp.name), dstIp(dp.dstIp) {};
-        /**
-         * get the name of this destination end point
-         * @return string name of destination end point
-         */
-        const string& getName() const { return name; };
-        /**
-         * set name of this destination point
-         * @param[in] name_ name of the destination end point
-         */
-        void setName(const string& name_)  { name = name_;};
+        DstEndPoint(const DstEndPoint& dp) : dstIp(dp.dstIp) {};
         /**
          * get the address of this destination end point
          * @return address address of this destination end point.
@@ -116,36 +67,27 @@ using namespace opflex::modb;
         const address& getAddress() const { return dstIp; };
 
     private:
-        string name;
         address dstIp;
-    };
+};
 
-    /**
-     * class to represent a source end point
-     */
-    class SourceEndPoint {
+/**
+ * class to represent a source end point
+ */
+class SourceEndPoint {
     public:
         /**
          * constructor takes a name and port
-         * @param[in] name name of localEp
-         * @param[in] port source port name on the vswitch
+         * @param[in] name_ name of localEp
+         * @param[in] port_ source port name on the vswitch
+         * @param[in] dir_ direction to be set
          */
-        SourceEndPoint(const string& name, const string& port);
+        SourceEndPoint(const string& name_, const string& port_, const unsigned char dir_) :
+            name(name_), port(port_), dir(dir_) {};
         /**
          * gets the name of the source end point
          * @return name of source end point
          */
         const string& getName() const { return name; };
-        /**
-         * gets the IP address of the source end point
-         * @return IP address of source end point
-         */
-        const address &getSrcEndPoint() const { return srcEndPoint; };
-        /**
-         * set srcEndPoint address
-         * @param[in] address_ IP address of the source end point
-         */
-        void setSrcEndPoint(const address& address_) { srcEndPoint = address_;};
         /**
          * gets the port name of the source end point
          * @return name of port source end point
@@ -156,32 +98,17 @@ using namespace opflex::modb;
          * @return a value from DirectionEnumT.
          */
         const unsigned char getDirection() const { return dir; };
-        /**
-         * set direction to one of gbp::DirectionEnumT values
-         * @param[in] dir_ direction to be set
-         */
-        void setDirection(const unsigned char dir_) { dir = dir_;};
-        /**
-         * gets the map of filters keyed by filter name
-         * @return a reference to a map of name to filters.
-         */
-        const unordered_map<string, filter_t>& getFilterSet() const {
-            return filters;
-        }
 
     private:
         string name;
-        address srcEndPoint;
         string port;
         unsigned char dir;
-        unordered_map<string, filter_t> filters;
+};
 
-    };
-
-    /**
-    * class to represent a span session.
-    */
-    class SessionState {
+/**
+ * class to represent a span session.
+ */
+class SessionState {
     public:
         /**
          * comparison criteria for adding to srcEndPoint set
@@ -196,10 +123,7 @@ using namespace opflex::modb;
              */
             bool operator()(std::shared_ptr<SourceEndPoint> src1,
                     std::shared_ptr<SourceEndPoint> src2) const {
-                if (src1->getPort().compare(src2->getPort()) == 0)
-                    return true;
-                else
-                    return false;
+                return src1->getPort() == src2->getPort();
             }
         };
         /**
@@ -224,26 +148,20 @@ using namespace opflex::modb;
         typedef unordered_set<shared_ptr<SourceEndPoint>, SrcHash, SrcCompare>
             srcEpSet;
         /**
-         * constructor that takes a SessionState as reference.
-         * @param s reference to a SessionState object
-         */
-        SessionState(const SessionState &s);
-        /**
          * constructor that takes a URI that points to a Session object
-         * @param uri URI to a Session object
-         * @param name name of Session object
+         * @param uri_ URI to a Session object
+         * @param name_ name of Session object
          */
-        SessionState(const URI& uri, const string& name);
+        SessionState(const URI& uri_, const string& name_) :
+            uri(uri_), name(name_),
+            adminState(modelgbp::platform::AdminStateEnumT::CONST_OFF),
+            version(modelgbp::span::ErspanVersionEnumT::CONST_V2) {};
+
         /**
          * gets the URI, which points to a Session object
          * @return a URI
          */
         const URI& getUri() const { return uri;};
-        /**
-         * set the URI to the one passed in
-         * @param uri_ URI to a Session object
-         */
-        void setUri(URI &uri_) { uri = uri_;};
 
         /**
          * add a destination end point to the internal map
@@ -322,14 +240,11 @@ using namespace opflex::modb;
         string name;
         uint8_t adminState;
         uint8_t version;
-        // mapping LocalEp to SourceEndPoint
-        // unordered_map<URI, shared_ptr<SourceEndPoint>> srcEndPoints;
 
         srcEpSet srcEndPoints;
         // mapping DstSummary to DstEndPoint
         unordered_map<URI, shared_ptr<DstEndPoint>> dstEndPoints;
-        unordered_map<string, filter_t> filters;
-    };
+};
 }
 
 #endif // SPANSESSIONSTATE_H
