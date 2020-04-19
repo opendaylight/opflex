3c6dd7e1715291ecf1ba5a4a721b1bb60cad5418
Tom Flynn <tom.flynn@gmail.com>
2020-04-19 01:13:50 -0700

    Enable prometheus support in Travis CI builds
    Signed-off-by: Tom Flynn <tom.flynn@gmail.com>
    Change-Id: I1ac619b592b845fd4688a0784fabc93e638d0b4d

--- .travis/travis-build.sh
+++ .travis/travis-build.sh
@@ -35,9 +35,25 @@ sudo cp -t "/usr/local/include/openvswitch/" include/*.h
 sudo find lib -name "*.h" -exec cp --parents -t "/usr/local/include/openvswitch/" {} \;
 popd
 
+git clone https://github.com/noironetworks/3rdparty-debian.git
+cp 3rdparty-debian/prometheus/prometheus-cpp.patch ~
+git clone https://github.com/jupp0r/prometheus-cpp.git
+pushd prometheus-cpp
+git checkout 9effb90b0c266316358680cbf862a8564eb2c2d4
+git submodule init
+git submodule update
+git apply ~/prometheus-cpp.patch
+mkdir _build && cd _build
+cmake .. -DBUILD_SHARED_LIBS=ON &> /dev/null
+make $make_args
+make install
+ls -l /usr/local/lib64
+mv /usr/local/lib64/libprometheus-cpp-* /usr/local/lib/
+popd
+
 pushd agent-ovs
 ./autogen.sh &> /dev/null
-./configure --enable-coverage &> /dev/null
+./configure --enable-coverage --enable-prometheus &> /dev/null
 make -j2
 sudo make install
 make check
