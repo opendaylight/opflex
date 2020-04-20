654c55a0d0fc41e3a46067e1b0588e682ff04a23
Tom Flynn <tom.flynn@gmail.com>
2020-04-20 00:16:45 -0700

    Add UT for SimStats
    Signed-off-by: Tom Flynn <tom.flynn@gmail.com>
    Change-Id: I8cfaaa97ef2792adaa802564423ee5eef9b5fb2e

--- agent-ovs/lib/test/SimStats_test.cpp
+++ agent-ovs/lib/test/SimStats_test.cpp
@@ -19,6 +19,7 @@ namespace opflexagent {
 class SimStatsFixture : public BaseFixture {
 public:
     SimStatsFixture() : BaseFixture(), epSource(&agent.getEndpointManager()), simStats(agent) {
+        simStats.start();
         shared_ptr<modelgbp::policy::Universe> pUniverse =
             modelgbp::policy::Universe::resolve(framework).get();
         Mutator mutator(framework, "policyreg");
@@ -31,20 +32,33 @@ public:
         simContract = simSpace->addGbpContract("test");
         auto simSubj = simContract->addGbpSubject("subj");
         auto simRule = simSubj->addGbpRule("rule");
+        simRule->setDirection(0);
         auto classifierTarget = simRule->addGbpRuleToClassifierRSrc("targetName");
         classifierTarget->setTargetL24Classifier(simClassifier->getURI());
         auto targetA = epg1->addGbpEpGroupToProvContractRSrc("targetA");
         targetA->setTargetContract(simContract->getURI());
         auto targetB = epg2->addGbpEpGroupToConsContractRSrc("targetB");
         targetB->setTargetContract(simContract->getURI());
+        auto targetC = epg1->addGbpEpGroupToIntraContractRSrc("targetC");
+        targetC->setTargetContract(simContract->getURI());
+
+        simSG = simSpace->addGbpSecGroup("abc");
+        auto sgSubj = simSG->addGbpSecGroupSubject("subj");
+        auto sgRule = sgSubj->addGbpSecGroupRule("rule");
+        sgRule->setDirection(0);
+        sgRule->setOrder(1);
+        auto sgRuleTarget = sgRule->addGbpRuleToClassifierRSrc("sgRuleTarget");
+        sgRuleTarget->setTargetL24Classifier(simClassifier->getURI());
         mutator.commit();
     }
 
     virtual ~SimStatsFixture() {
+        simStats.stop();
     }
 
     shared_ptr<modelgbp::policy::Space> simSpace;
-    shared_ptr<modelgbp::gbp::Contract> simContract;
+    shared_ptr<Contract> simContract;
+    shared_ptr<SecGroup> simSG;
     MockEndpointSource epSource;
     SimStats simStats;
 };
@@ -66,8 +80,36 @@ BOOST_FIXTURE_TEST_CASE(interface_counters, SimStatsFixture ) {
     simStats.updateInterfaceCounters();
 }
 
+static bool hasConsContracts(const shared_ptr<EpGroup>& epg, std::vector<OF_SHARED_PTR<EpGroupToConsContractRSrc> > consRefs) {
+    epg->resolveGbpEpGroupToConsContractRSrc(consRefs);
+    return !consRefs.empty();
+}
+
+static bool hasRules(PolicyManager& pm, const shared_ptr<Contract>& contract, list<shared_ptr<PolicyRule> >& rules) {
+    pm.getContractRules(contract->getURI(), rules);
+    return !rules.empty();
+}
+
+bool hasSgRules(PolicyManager& pm, const shared_ptr<SecGroup>& sg, list<shared_ptr<PolicyRule> >& rules) {
+    pm.getSecGroupRules(sg->getURI(), rules);
+    return !rules.empty();
+}
+
 BOOST_FIXTURE_TEST_CASE(contract_counters, SimStatsFixture ) {
     simStats.contractUpdated(simContract->getURI());
+    WAIT_FOR(simSpace->resolveGbpEpGroup("b"), 500);
+    auto epgB = simSpace->resolveGbpEpGroup("b");
+    std::vector<OF_SHARED_PTR<EpGroupToConsContractRSrc> > consRefs;
+    WAIT_FOR(hasConsContracts(epgB.get(), consRefs), 500);
+    list<shared_ptr<PolicyRule> > rules;
+    WAIT_FOR(hasRules(agent.getPolicyManager(), simContract, rules), 500);
     simStats.updateContractCounters();
+}
+
+BOOST_FIXTURE_TEST_CASE(sg_counters, SimStatsFixture ) {
+    simStats.secGroupUpdated(simSG->getURI());
+    list<shared_ptr<PolicyRule> > rules;
+    WAIT_FOR(hasSgRules(agent.getPolicyManager(), simSG, rules), 500);
+    simStats.updateSecGrpCounters();
 }
 }
