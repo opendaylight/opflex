021d9218d800ea5d5df38824ac187838b2e89586
Tom Flynn <tom.flynn@gmail.com>
2020-05-07 19:49:16 -0700

    Address issue with creating multiple mirrors for ERSPAN
    Signed-off-by: Tom Flynn <tom.flynn@gmail.com>
    Change-Id: I0eabea43ee4984f0583dd08d70844150fa4240b3

--- agent-ovs/ovs/SpanRenderer.cpp
+++ agent-ovs/ovs/SpanRenderer.cpp
@@ -220,9 +220,9 @@ namespace opflexagent {
         createMirror(seSt->getName(), srcPort, dstPort);
     }
 
-    bool SpanRenderer::deleteMirror(const string& sess) {
-        LOG(DEBUG) << "deleting mirror " << sess;
-        if (!jRpc->deleteMirror(switchName)) {
+    bool SpanRenderer::deleteMirror(const string& sessionName) {
+        LOG(DEBUG) << "deleting mirror " << sessionName;
+        if (!jRpc->deleteMirror(switchName, sessionName)) {
             LOG(DEBUG) << "Unable to delete mirror";
             return false;
         }
